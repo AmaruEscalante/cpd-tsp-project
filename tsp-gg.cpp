a45cebfac03aea55707f9fa1a0a43874bfd28f15
amaruescalante <amaruescalante@gmail.com>
2021-07-20 02:36:49 -0400

    first part parallelized

--- tsp-gg.cpp
+++ tsp-gg.cpp
@@ -2,12 +2,13 @@
 // using Branch and Bound.
 // Geek for Geeks
 #include <bits/stdc++.h>
+#include <omp.h>
 using namespace std;
 const int N = 4;
 
 // final_path[] stores the final solution ie, the
 // path of the salesman.
-int final_path[N+1];
+int final_path[N + 1];
 
 // visited[] keeps track of the already visited nodes
 // in a particular path
@@ -20,7 +21,7 @@ int final_res = INT_MAX;
 // the final solution
 void copyToFinal(int curr_path[])
 {
-	for (int i=0; i<N; i++)
+	for (int i = 0; i < N; i++)
 		final_path[i] = curr_path[i];
 	final_path[N] = curr_path[0];
 }
@@ -30,8 +31,8 @@ void copyToFinal(int curr_path[])
 int firstMin(int adj[N][N], int i)
 {
 	int min = INT_MAX;
-	for (int k=0; k<N; k++)
-		if (adj[i][k]<min && i != k)
+	for (int k = 0; k < N; k++)
+		if (adj[i][k] < min && i != k)
 			min = adj[i][k];
 	return min;
 }
@@ -41,7 +42,7 @@ int firstMin(int adj[N][N], int i)
 int secondMin(int adj[N][N], int i)
 {
 	int first = INT_MAX, second = INT_MAX;
-	for (int j=0; j<N; j++)
+	for (int j = 0; j < N; j++)
 	{
 		if (i == j)
 			continue;
@@ -52,7 +53,7 @@ int secondMin(int adj[N][N], int i)
 			first = adj[i][j];
 		}
 		else if (adj[i][j] <= second &&
-				adj[i][j] != first)
+				 adj[i][j] != first)
 			second = adj[i][j];
 	}
 	return second;
@@ -70,16 +71,16 @@ void TSPRec(int adj[N][N], int curr_bound, int curr_weight,
 {
 	// base case is when we have reached level N which
 	// means we have covered all the nodes once
-	if (level==N)
+	if (level == N)
 	{
 		// check if there is an edge from last vertex in
 		// path back to the first vertex
-		if (adj[curr_path[level-1]][curr_path[0]] != 0)
+		if (adj[curr_path[level - 1]][curr_path[0]] != 0)
 		{
 			// curr_res has the total weight of the
 			// solution we got
 			int curr_res = curr_weight +
-					adj[curr_path[level-1]][curr_path[0]];
+						   adj[curr_path[level - 1]][curr_path[0]];
 
 			// Update final result and final path if
 			// current result is better.
@@ -94,25 +95,27 @@ void TSPRec(int adj[N][N], int curr_bound, int curr_weight,
 
 	// for any other level iterate for all vertices to
 	// build the search space tree recursively
-	for (int i=0; i<N; i++)
+	for (int i = 0; i < N; i++)
 	{
 		// Consider next vertex if it is not same (diagonal
 		// entry in adjacency matrix and not visited
 		// already)
-		if (adj[curr_path[level-1]][i] != 0 &&
+		if (adj[curr_path[level - 1]][i] != 0 &&
 			visited[i] == false)
 		{
 			int temp = curr_bound;
-			curr_weight += adj[curr_path[level-1]][i];
+			curr_weight += adj[curr_path[level - 1]][i]; // Add weight of edge
 
 			// different computation of curr_bound for
 			// level 2 from the other levels
-			if (level==1)
-			curr_bound -= ((firstMin(adj, curr_path[level-1]) +
-							firstMin(adj, i))/2);
+			if (level == 1)
+				curr_bound -= ((firstMin(adj, curr_path[level - 1]) +
+								firstMin(adj, i)) /
+							   2);
 			else
-			curr_bound -= ((secondMin(adj, curr_path[level-1]) +
-							firstMin(adj, i))/2);
+				curr_bound -= ((secondMin(adj, curr_path[level - 1]) +
+								firstMin(adj, i)) /
+							   2);
 
 			// curr_bound + curr_weight is the actual lower bound
 			// for the node that we have arrived on
@@ -124,18 +127,18 @@ void TSPRec(int adj[N][N], int curr_bound, int curr_weight,
 				visited[i] = true;
 
 				// call TSPRec for the next level
-				TSPRec(adj, curr_bound, curr_weight, level+1,
-					curr_path);
+				TSPRec(adj, curr_bound, curr_weight, level + 1,
+					   curr_path);
 			}
 
 			// Else we have to prune the node by resetting
 			// all changes to curr_weight and curr_bound
-			curr_weight -= adj[curr_path[level-1]][i];
+			curr_weight -= adj[curr_path[level - 1]][i];
 			curr_bound = temp;
 
 			// Also reset the visited array
 			memset(visited, false, sizeof(visited));
-			for (int j=0; j<=level-1; j++)
+			for (int j = 0; j <= level - 1; j++)
 				visited[curr_path[j]] = true;
 		}
 	}
@@ -144,7 +147,7 @@ void TSPRec(int adj[N][N], int curr_bound, int curr_weight,
 // This function sets up final_path[]
 void TSP(int adj[N][N])
 {
-	int curr_path[N+1];
+	int curr_path[N + 1];
 
 	// Calculate initial lower bound for the root node
 	// using the formula 1/2 * (sum of first min +
@@ -154,14 +157,16 @@ void TSP(int adj[N][N])
 	memset(curr_path, -1, sizeof(curr_path));
 	memset(visited, 0, sizeof(curr_path));
 
-	// Compute initial bound
-	for (int i=0; i<N; i++)
+// Compute initial bound
+// Paralellize current_bound computation with a reduce operation
+#pragma omp parallel for reduction(+ \
+								   : curr_bound)
+	for (int i = 0; i < N; i++)
 		curr_bound += (firstMin(adj, i) +
-					secondMin(adj, i));
+					   secondMin(adj, i));
 
-	// Rounding off the lower bound to an integer
-	curr_bound = (curr_bound&1)? curr_bound/2 + 1 :
-								curr_bound/2;
+	// Rounding off the lower bound to an integer value
+	curr_bound = (int)floor(curr_bound / 2);
 
 	// We start at vertex 1 so the first vertex
 	// in curr_path[] is 0
@@ -176,18 +181,20 @@ void TSP(int adj[N][N])
 // Driver code
 int main()
 {
+	// Initialize OMP environment
+	omp_set_num_threads(4);
 	//Adjacency matrix for the given graph
-	int adj[N][N] = { {0, 10, 15, 20},
+	int adj[N][N] = {
+		{0, 10, 15, 20},
 		{10, 0, 35, 25},
 		{15, 35, 0, 30},
-		{20, 25, 30, 0}
-	};
+		{20, 25, 30, 0}};
 
 	TSP(adj);
 
 	printf("Minimum cost : %d\n", final_res);
 	printf("Path Taken : ");
-	for (int i=0; i<=N; i++)
+	for (int i = 0; i <= N; i++)
 		printf("%d ", final_path[i]);
 
 	return 0;
