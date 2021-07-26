41b6a92c91183e95144d70ab4ad932e8643d192f
JamesAtachagua <james.atachagua@utec.edu.pe>
2021-07-25 23:33:58 -0300

    refact run tests script

--- tests/run.sh
+++ tests/run.sh
@@ -2,18 +2,20 @@
 
 echo "Init test of TSP Proyect at: " `date +\%Y-\%m-\%d--\%H-\%M`
 
-LIST=$(ls tests/*.txt | cut -f 1 -d '.' | cut -d_ -f2-)
-# FILES=$(ls tests/*.txt | cut -d/ -f2-)
+LIST=$(ls tests/matrices/*.txt | cut -f 1 -d '.' | cut -d_ -f2-)
 
-cd tests
+cd tests/matrices
 
 # for loop 
 for i in $LIST
 do
     echo " "
-    echo "Test: $i"
+    echo " ######################################################### "
+    echo "Test with a matrix with $i elements"
     FILE=$(ls *_$i.txt)
-    echo "Execute " "./../build/main-$i.out" " $FILE"
-    ./../build/main-$i.out "$FILE"
+    echo "Execute " "./../build/tests/main-$i.out" " $FILE"
+    ./../../build/tests/main-$i.out "$FILE"
     echo "Test: $i - OK"
+    echo " ######################################################### "
+    echo " "
 done
