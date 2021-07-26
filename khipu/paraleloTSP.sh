89d3acc50565eabdc695a5a17e38556ff29e3104
JamesAtachagua <james.atachagua@utec.edu.pe>
2021-07-26 02:26:50 -0300

    parallel script

--- khipu/paraleloTSP.sh
+++ khipu/paraleloTSP.sh
@@ -6,10 +6,10 @@
 #SBATCH --output=tsp-par-20.log
 
 # To compile
-# module load gcc/5.5.0
-# g++ -std=c++17 ../norecursivo/paralelo/par-dfs-v2-tsp.cpp -fopenmp -lpthread -o par-dfs-v2-tsp-20 -DSIZE=20
+module load gcc/5.5.0
+g++ -std=c++17 ../norecursivo/paralelo/par-dfs-v2-tsp.cpp -fopenmp -lpthread -o par-dfs-v2-tsp-20 -DSIZE=20
 # module unload gcc/5.5.0
 
-module load gcc/5.5.0
+# module load gcc/5.5.0
 ./par-dfs-v2-tsp-20 ../tests/matrices/matrix_20.txt.bk
 module unload gcc/5.5.0
