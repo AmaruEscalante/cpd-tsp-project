d0433e3eb3b0f5b0c56f78f50332ec7d27b7ca58
JamesAtachagua <james.atachagua@utec.edu.pe>
2021-07-27 06:35:26 -0300

    add parallel algoritm

--- src/Algoritmo/tsp.cpp
+++ src/Algoritmo/tsp.cpp
@@ -45,6 +45,7 @@
 #include <chrono>
 #include <cmath>
 #include <iomanip>
+#include <omp.h>
 
 #include "tsp.h"
 #include "utils.h"
@@ -195,6 +196,93 @@ int tsp()
     // Print time taken
     auto time = std::chrono::duration_cast<std::chrono::nanoseconds>(stop - start).count();
     print_time(time, false);
+
+    return 0;
+}
+
+int tsp_par()
+{
+    /* Create stack */
+    stack<tour_t *> root_stack;
+    stack<tour_t *> my_stack;
+
+    int thread_count = 4;
+    // Initialize threads
+    omp_set_num_threads(thread_count);
+
+    // Initialize best tour
+    vector<int> best_tour_root;
+    best_tour = newTour(best_tour_root, INT_MAX);
+
+    // Initialize first tour
+    vector<int> root_cities;
+    root_cities.push_back(0);
+    tour_t *root_tour = newTour(root_cities, 0);
+    //root_stack.push(root);
+
+    double start = omp_get_wtime();
+
+    // Initialize thread-local best tour
+#pragma omp parallel private(my_stack)
+    {
+#pragma omp for
+        for (int i = 1; i < n; i++)
+        {
+            tour_t *local_tour = newTour(root_cities, 0);
+            add_city(local_tour, i);
+            my_stack.push(local_tour);
+        }
+        // Main loop
+        while (!my_stack.empty())
+        {
+            tour_t *curr_tour = my_stack.top();
+            my_stack.pop();
+            if (curr_tour->cities.size() == n)
+            {
+                if (is_best_tour(curr_tour))
+                {
+#pragma omp critical
+                    {
+                        if (is_best_tour(curr_tour))
+                        {
+                            update_best_tour(curr_tour);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int nbr = n - 1; nbr >= 1; nbr--)
+                {
+                    if (feasible(curr_tour, nbr))
+                    {
+                        add_city(curr_tour, nbr);
+                        push_copy(my_stack, curr_tour);
+                        remove_city(curr_tour, nbr);
+                    }
+                }
+            }
+            delete curr_tour;
+        }
+    }
+
+    double stop = omp_get_wtime();
+
+    // Output best tour
+    std::cout << "Best tour is: " << endl;
+    int i = 0;
+    for (i = 0; i < best_tour->cities.size(); i++) {
+        std::cout << best_tour->cities[i] << " ";
+        result_path[i] = best_tour->cities[i];
+    }
+    std::cout << best_tour->cities[0] << " ";
+    result_path[i] = best_tour->cities[0];
+    std::cout << std::endl;
+    std::cout << "Best tour cost is: " << best_tour->cost << std::endl;
+
+    // Print time taken
+    double time = stop - start;
+    print_time(time, true);
 
     return 0;
 }
--- src/Algoritmo/utils.cpp
+++ src/Algoritmo/utils.cpp
@@ -7,10 +7,12 @@ using namespace std;
 void print_time(double time, bool par){
     cout << "----------------------------------------- " << endl;
     if (par) {
+        std::cout << std::fixed << std::setprecision(0);
+        cout << "Time taken is: " << time * pow(10, 9) << " nano seconds" << endl;
         std::cout << std::fixed << std::setprecision(9);
-        cout << "Time taken is: " << time * pow(10, -9) << " nano seconds" << endl;
         cout << "Time taken is: " << time << " seconds" << endl;
     } else {
+        std::cout << std::fixed << std::setprecision(0);
         cout << "Time taken is: " << time << " nano seconds" << endl;
         std::cout << std::fixed << std::setprecision(9);
         cout << "Time taken is: " << time * pow(10, -9) << " seconds" << endl;
