a12fcc30eab25d691a3a223c076351a11895cd8b
amaruescalante <amaruescalante@gmail.com>
2021-07-27 03:00:03 -0400

    fix(path_to_json): path to json file has been moved to root of src/

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,14 @@
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     cout << "================================================" << endl;
     cout << "MENU TSP TESTER" << endl;
     cout << "================================================" << endl;
 
     DistrictParser<string, int> districtsparse(
-        "/Users/jamesatachagua/Desktop/Proyectos/"
-        "cpd-tsp-project/src/Parser/Data/maps_api_google.json");
+        "maps_api_google.json");
     cout << endl;
 
     districtsparse.execute_TSP();
