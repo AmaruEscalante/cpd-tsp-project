ce39e18862f5ee4df2a49f0da9734961e6924635
amaruescalante <amaruescalante@gmail.com>
2021-07-28 11:11:24 -0400

    updated matrices and script to run tests

--- khipu/paraleloTSP_N20.sh
+++ khipu/paraleloTSP_N20.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -J tsp-par-N20
 #SBATCH -N 1 # poner mas porque se va a ejecutar mas veces
-#SBATCH --tasks-per-node=40 # Cantidad de CPUs
-#SBATCH --mem-per-cpu=2GB
+#SBATCH --tasks-per-node=32 # Cantidad de CPUs
+#SBATCH --mem=20GB
 #SBATCH --output=tsp-par-m20-p2-p4-p8-p16-p20.log
 
 # To compile
@@ -12,23 +12,23 @@ module load gcc/5.5.0
 # Parallel processes
 echo 'TSP con 2 Threads para matrix_20.txt'
 g++ -std=c++17 ../norecursivo/paralelo/par-dfs-v2-tsp.cpp -fopenmp -lpthread -o par-dfs-v2-tsp -DSIZE=20
-./par-dfs-v2-tsp1 ../tests/matrices/matrix_20.txt.bk 2 &
+./par-dfs-v2-tsp ../tests/matrices/matrix_20.txt.bk 2 > process1.log &
 
 echo 'TSP con 4 Threads para matrix_20.txt'
 g++ -std=c++17 ../norecursivo/paralelo/par-dfs-v2-tsp.cpp -fopenmp -lpthread -o par-dfs-v2-tsp -DSIZE=20
-./par-dfs-v2-tsp2 ../tests/matrices/matrix_20.txt.bk 4 &
+./par-dfs-v2-tsp ../tests/matrices/matrix_20.txt.bk 4 > process2.log &
 
 echo 'TSP con 8 Threads para matrix_20.txt'
 g++ -std=c++17 ../norecursivo/paralelo/par-dfs-v2-tsp.cpp -fopenmp -lpthread -o par-dfs-v2-tsp -DSIZE=20
-./par-dfs-v2-tsp3 ../tests/matrices/matrix_20.txt.bk 8 &
+./par-dfs-v2-tsp ../tests/matrices/matrix_20.txt.bk 8 > process3.log &
 
 echo 'TSP con 16 Threads para matrix_20.txt'
 g++ -std=c++17 ../norecursivo/paralelo/par-dfs-v2-tsp.cpp -fopenmp -lpthread -o par-dfs-v2-tsp -DSIZE=20
-./par-dfs-v2-tsp4 ../tests/matrices/matrix_20.txt.bk 16 &&
+./par-dfs-v2-tsp ../tests/matrices/matrix_20.txt.bk 16 > process4.log &&
 # Last process runs alone
-echo 'TSP con 20 Threads para matrix_20.txt'
+echo 'TSP con 20 Threads para matrix_20.txt' &&
 g++ -std=c++17 ../norecursivo/paralelo/par-dfs-v2-tsp.cpp -fopenmp -lpthread -o par-dfs-v2-tsp -DSIZE=20
-./par-dfs-v2-tsp5 ../tests/matrices/matrix_20.txt.bk 20
+./par-dfs-v2-tsp ../tests/matrices/matrix_20.txt.bk 20 > process5.log
 
 wait
 
